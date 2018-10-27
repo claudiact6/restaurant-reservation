c477b68425c23344b328bf69ac888499031bc3a2
kimk <38691828+claudiact6@users.noreply.github.com>
2018-10-27 12:41:16 -0500

    basic functionality

---
+++ server.js
@@ -0,0 +1,63 @@
+//dependencies
+
+var express = require("express");
+var path = require("path");
+
+//express setup 
+
+var app = express();
+var PORT = 3000;
+
+//express parsing
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//data
+
+var tables = []
+var waitlist = []
+
+//API routing
+
+app.get("/api/tables", function(req, res) {
+    return res.json(tables);
+});
+
+app.get("/api/waitlist", function(req, res) {
+    return res.json(waitlist);
+});
+
+//html pages routing
+app.get("/", function(req, res) {
+    return res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/tables", function(req, res) {
+    return res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reserve", function(req, res) {
+    return res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+//take in reservation info
+
+app.post("/api", function(req, res) {
+    console.log("Tables elngth",tables.length);
+    var resData = req.body;
+    if (tables.length < 5) {
+        tables.push(resData);
+        res.json(resData);
+    } else {
+        waitlist.push(resData);
+        res.json(resData);
+    };
+});
+
+//start server
+
+app.listen(PORT, function() {
+    console.log("Listening on port",PORT);
+});
+
